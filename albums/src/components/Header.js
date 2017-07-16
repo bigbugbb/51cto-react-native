286dcbebfe74e5aab6327ed99364977d7e181e8a
bigbugbb <bigbugbb@gmail.com>
2017-07-16 17:13:34 +0800

    add style to header text

--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -3,7 +3,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // 2. 创建Header component
-const Header = () => <Text>专辑</Text>
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>专辑</Text>
+}
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+}
 
 // 3. 导出Header component
 export default Header;
