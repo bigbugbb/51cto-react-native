df81b6588845745ac0125bdf40dbccaeffdd3db2
bigbugbb <bigbugbb@gmail.com>
2017-07-18 01:27:53 +0800

    load oss data in componentWillMount with fetch api

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,10 +1,21 @@
 // 1. 导入React和React Native
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/Header';
 
 // 2. 创建我们的Component
-const App = () => <Header headerText="专辑" />
+class App extends Component {
+  componentWillMount() {
+    fetch('http://albums-app-dat.oss-cn-hangzhou.aliyuncs.com/albums.json')
+      .then((response) => response.json())
+      .then((responseJson) => console.log(responseJson))
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    return <Header headerText="专辑" />
+  }
+}
 
 // 3. 渲染Component
 AppRegistry.registerComponent("albums", () => App);
--- albums/index.ios.js
+++ albums/index.ios.js
@@ -6,9 +6,12 @@ import Header from './src/components/Header';
 // 2. 创建我们的Component
 class App extends Component {
   componentWillMount() {
-
+    fetch('http://albums-app-dat.oss-cn-hangzhou.aliyuncs.com/albums.json')
+      .then((response) => response.json())
+      .then((responseJson) => console.log(responseJson))
+      .catch((error) => console.log(error));
   }
-  
+
   render() {
     return <Header headerText="专辑" />
   }
