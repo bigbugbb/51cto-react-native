6b5c9d0c2e86e039e838189e7c771de39001541b
bigbugbb <bigbugbb@gmail.com>
2017-07-17 21:21:17 +0800

    change App component from functional component to class based component

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,10 +1,18 @@
 // 1. 导入React和React Native
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/Header';
 
 // 2. 创建我们的Component
-const App = () => <Header headerText="专辑" />
+class App extends Component {
+  componentWillMount() {
+
+  }
+  
+  render() {
+    return <Header headerText="专辑" />
+  }
+}
 
 // 3. 渲染Component
 AppRegistry.registerComponent("albums", () => App);
